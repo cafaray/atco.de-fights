0b03164f3bbf9ac59c6da54db5efe5db3fe32926
Carlos Alberto Farias Ayala <cafaray@gmail.com>
2018-06-03 22:28:07 +0200

    Upgrade exercise, it's wrong

--- greatestCommonPrimeDivisor.py
+++ greatestCommonPrimeDivisor.py
@@ -1,10 +1,10 @@
 def greatestCommonPrimeDivisor(a, b):
-    x=-1
-    for y in range(2,min(a,b)+1):
-        if a%x==b%x==0:
-            y=x
-        while a%x==0:
-            a//=x
-        while b%x==0:
-            b//=x
-    return y
+#    x=-1
+#    for y in range(2,min(a,b)+1):
+#        if a%x==b%x==0:
+#            y=x
+#        while a%x==0:
+#            a//=x
+#        while b%x==0:
+#            b//=x
+#    return y
