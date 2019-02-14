70d9943946bdbc481fe30469199a9ba2e0898797
Carlos Alberto Farias Ayala <cafaray@gmail.com>
2019-02-14 12:58:01 +0100

    new cases

---
+++ countSumOfTwoRepresentations.py
@@ -0,0 +1,12 @@
+def countSumOfTwoRepresentations(n, l, r):
+#    if l+r>n: return 0
+#    c=0    
+#    while l<=r:
+#        if l+r==n:
+#            c+=1
+#        r-=1
+#        l+=1
+#    return c
+#    return min([(n//2-l), (r-(n//2))]) + (n%2==0) if (n//2) >= l and (n//2)<=r else 1-(n%2==0)
+
+    return sum(1 for a in range(l, r+1) if l <= a <= n - a <= r)
