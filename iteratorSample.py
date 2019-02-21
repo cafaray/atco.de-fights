4841a96c2193223c22f7c2f14e64f2f0dd5e38e8
Carlos Alberto Farias Ayala <cafaray@gmail.com>
2019-02-21 11:11:32 +0100

    fixing iterator sample

--- iteratorSample.py
+++ iteratorSample.py
@@ -3,30 +3,33 @@ def calkinWilfSequence(number):
         last = ([1,1],[1,1])
         r = True
         while True:
-            print('before yield', last, r)
+            #print('before yield', last, r)
             yield last[1 if r else 0]
             if r:
-                last = [[last[0][0], last[0][0]], [last[1][0] + last[1][1], last[1][0]]] 
+                last = [[last[0][0], last[0][0]+last[0][1]], [last[1][0], last[1][1]]] 
             else:
-                last = [last[0][0] + last[0][1], last[0][0], [last[1][0], last[1][1]]]       
+                last = [[last[0][0], last[0][1]], [last[1][0]+last[1][1], last[1][0]]]       
             r = not r
-            print('after yield')
+            #print('after yield')
 
     gen = fractions()
     res = 0
     while True:
         n = next(gen)
+        print(n)
         if n == number:
             break
         res += 1
     return res
 
 
-print(calkinWilfSequence([1,3]))
+print("Assertion result on [1,3] = ", calkinWilfSequence([1,3])==3)
+print("Assertion result on [14,3] = ", calkinWilfSequence([14,3])==110)
+print("Assertion result on [7,13] = ", calkinWilfSequence([7,13])==129)
 
-mytuple = ("apple", "banana", "cherry")
-myit = iter(mytuple)
+#mytuple = ("apple", "banana", "cherry")
+#myit = iter(mytuple)
 
-print(next(myit))
-print(next(myit))
-print(next(myit))
+#print(next(myit))
+#print(next(myit))
+#print(next(myit))
