d860d237fdfd750a07b425039c444ed3d6c2a1f3
Alberto Farias Ayala <cafaray@gmail.com>
2019-04-24 18:53:26 +0200

    comparator in class object

--- sortCodesignalUsers.py
+++ sortCodesignalUsers.py
@@ -1,52 +1,63 @@
+from functools import total_ordering
 def sortCodesignalUsers(users):
     res = [CodeSignalUser(*user) for user in users]
     res.sort(reverse=True)
     return list(map(str, res))
 
-
+@total_ordering
 class CodeSignalUser:
-        
-    #def mycmp(o1, o2):
-        #return o1[2]-o2[2]
-        
-    def __gt__(self, other):
-        if self.xp>other.xp:
-            return self
-        elif self.xp<other.xp:
-            return other
-        else:
-            if self.id < other.id:
-                return self
-            else:
-                return other
-    
-    def __lt__(self, other):
-        if self.xp<other.xp:
-            return self
-        elif self.xp>other.xp:
-            return other
-        else:
-            if self.id < other.id:
-                return self
-            else:
-                return other
 
     def __eq__(self, other):
-        if self.xp==other.xp:
+        return (self.xp == other.xp)
+
+    def __ne__(self, other):
+        return not (self==other)    
+
+    #def __repr__(self):
+    #    return "%s %s" % (self.first, self.last)
+
+    def __lt__(self, other):
+        if self.xp < other.xp:
+            return True
+        elif self.xp == other.xp:
+            if self.id > other.id:
+                return True            
+        return False
+        
+    def __gt__(self, other):
+        if self.xp > other.xp:
+            return True
+        elif self.xp == other.xp:
             if self.id < other.id:
-                return self
-            else:
-                return other
+                return True            
+        return False
+    
+    def __ge__(self, other):
+        if self.xp >= other.xp:
+            return True
+        elif self.xp >= other.xp:
+            if self.id < other.id:
+                return True            
+        return False
+    
+    def __le__(self, other):
+        if self.xp <= other.xp:
+            return True
+        elif self.xp <= other.xp:
+            if self.id < other.id:
+                return True            
+        return False
 
     def __str__(self):
         return self.level
 
     def __init__(self, level, id, xp):
         self.level = level
-        self.id = id
-        self.xp = xp
+        self.id = int(id)
+        self.xp = int(xp)
 
 users = [["warrior", "1", "1050"],
          ["Ninja!",  "21", "995"],
-         ["recruit", "3", "995"]]
+         ["recruit", "3", "995"],
+         ["recruit2", "31", "1993"]]
 print(sortCodesignalUsers(users))
