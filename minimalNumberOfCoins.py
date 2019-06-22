3634b704afb6ce51e9fa100e09a518296d8b085a
Carlos Alberto Farias Ayala <cafaray@gmail.com>
2019-06-22 13:32:54 +0200

    new challenge excercise, minimalNumberOfCoins

--- minimalNumberOfCoins.py
+++ minimalNumberOfCoins.py
@@ -1,8 +1,9 @@
-# def minimalNumberOfCoins(coins, price):
-params=eval(dir()[0])
-c,p,m=params[0], params[1],0
-for i in range(len(c)-1,-1,-1):
-    m+=p//c[i]
-    p=p%c[i]
-    if p<=0: break
-return m
+def minimalNumberOfCoins(coins, price):
+#params=eval(dir()[0])
+#c,p,m=params[0], params[1],0
+    c,p,m=coins,price,0
+    for i in range(len(c)-1,-1,-1):
+        m+=p//c[i]
+        p=p%c[i]
+        if p<=0: break
+    return m
