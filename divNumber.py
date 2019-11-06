67da8820187a276131e60341abba78536465f678
Alberto Farias Ayala <cafaray@gmail.com>
2019-11-06 11:01:56 +0100

    upgrade an old excersise as challenge

--- divNumber.py
+++ divNumber.py
@@ -18,3 +18,14 @@ def divNumber(k, l, r):
         if divisor == k:
             ans += 1
     return ans
+
+def divNumber(k, l, r):
+    f=0
+    for n in range(l,r+1):
+        d,i=0,1
+        while i<=n:
+            if n%i==0:d+=1
+            if d>k:break
+            i+=1
+        if d==k:f+=1
+    return f
