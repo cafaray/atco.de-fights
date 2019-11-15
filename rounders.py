c9cc856e88b0db4749a4cf22201af551b112493d
Carlos Farias <carlos.farias@gft.com>
2019-11-15 22:47:50 +0100

    new solution for rounders challenge

--- rounders.py
+++ rounders.py
@@ -1,4 +1,4 @@
-def rounders(value):
+def rounders_vs(value):
     svalue = str(value)[::-1]
     for s in range(0,len(svalue)-1):
         #print(s, '=', int(svalue[s]))
@@ -15,3 +15,47 @@ def rounders(value):
         svalue = str(value)[::-1]
         #print('nuevo:',svalue)
     return value
+
+
+def rounders(n):
+    c=1
+    f0=(10**c)
+    while n//f0>0:                
+        f1=(10**(c-1))
+        i=n%f0//f1
+        if i>=5:
+            n+=(f0)
+        n-=i*(f1)
+        c+=1  
+        f0=(10**c)              
+    return n
+
+n=15
+e=20
+r=rounders(n)
+print('Assertion ', e, "==", r, ": ", e==r)
+
+n=1234
+e=1000
+r=rounders(n)
+print('Assertion ', e, "==", r, ": ", e==r)
+
+n=1445
+e=2000
+r=rounders(n)
+print('Assertion ', e, "==", r, ": ", e==r)
+
+n=10
+e=10
+r=rounders(n)
+print('Assertion ', e, "==", r, ": ", e==r)
+
+n=7001
+e=7000
+r=rounders(n)
+print('Assertion ', e, "==", r, ": ", e==r)
+
+n=99
+e=100
+r=rounders(n)
+print('Assertion ', e, "==", r, ": ", e==r)
