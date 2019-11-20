0522817074c67ee66fbe07e65f1f8ee0819a3de5
Alberto Farias Ayala <cafaray@gmail.com>
2019-11-20 09:35:51 +0100

    upgrade some challenges

--- depositProfit.py
+++ depositProfit.py
@@ -6,3 +6,14 @@ def depositProfit(deposit, rate, threshold):
         print(x, nd)
         if nd >= threshold:
             return x
+
+# best rate time:
+
+data=eval(dir()[0])
+data[1]=(data[1]/100)+1
+data[0]*=(data[1])
+x=1
+while data[0]<data[2]:        
+    data[0]*=(data[1])        
+    x+=1
+return x
