eb85e7d2c0e147fcc8ca3c9199b578fd9aea3572
Alberto Farias Ayala <cafaray@gmail.com>
2019-11-20 09:36:22 +0100

    upgrade some challenges

--- magicalWell.py
+++ magicalWell.py
@@ -5,3 +5,12 @@ def magicalWell(a, b, n):
         a = a+1
         b = b+1
     return m
+
+N=eval(dir()[0])
+m,s=1,0
+while m<=N[2]:
+    s+=N[0]*N[1]
+    N[0]+=1
+    N[1]+=1
+    m+=1
+return s
