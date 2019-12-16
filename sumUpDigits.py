4999149e042627056573729957bd909b1b4caf2b
Alberto Farias Ayala <cafaray@gmail.com>
2019-12-16 17:02:06 +0100

    solve challenge with a simple re

--- sumUpDigits.py
+++ sumUpDigits.py
@@ -4,3 +4,8 @@ def sumUpDigits(inputString):
         if c>='1' and c <= '9':
             suma += int(c)
     return suma
+
+# Another way using re
+s,l=0,re.compile('[0-9]').findall(eval(dir()[0])[0])
+for x in l: s+=(int(x))
+print(s)
