7ad1e30361ecc7d3dd3855967df890feb4ef6696
Alberto Farias Ayala <cafaray@gmail.com>
2019-12-23 11:29:39 +0100

    fix an old challenge for subset numbers over n numbres

--- divideAsLongAsPossible.py
+++ divideAsLongAsPossible.py
@@ -1,4 +1,9 @@
 def divideAsLongAsPossible(n, d):
+    while not n%d:
+        n/=d
+    return n
+    
+def divideAsLongAsPossible_old(n, d):
     cnt = 1
     nn = n
     for x in range(n):
--- divisorsSubset.py
+++ divisorsSubset.py
@@ -1,4 +1,20 @@
 def divisorsSubset(subset, n):    
+    ss=sorted(subset)
+    i=ss[0]
+    c=0
+    while i<=n:
+        inside=True
+        for x in ss[::-1]:
+            if i%x!=0:
+                inside=False
+                break
+        if inside:
+            c+=1
+        i+=1
+    return c 
+
+
+def divisorsSubset_old(subset, n):    
     c=0
     for i in range(1,n+1):
         l=True
