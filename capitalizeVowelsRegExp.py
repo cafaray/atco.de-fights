21699c02dbd0a27054658715f993448094432cb8
Alberto Farias Ayala <cafaray@gmail.com>
2019-12-24 08:09:56 +0100

    challenge for regex, really did it

--- capitalizeVowelsRegExp.py
+++ capitalizeVowelsRegExp.py
@@ -1,3 +1,10 @@
 def capitalizeVowelsRegExp(input):
+    def vowel(m):
+        v=m.group()
+        return v.upper()
+    p=re.compile('a|e|i|o|u|y')
+    return p.sub(vowel, input)
+
+def capitalizeVowels(input):
     vovel_list = ['a', 'e', 'i', 'o', 'u', 'y']
     return "".join([char.upper() if char in vovel_list else char for char in input])
